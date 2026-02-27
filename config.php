d9fcd7eb9ed545f78d2efd1046ef7d4d8765e66a
jkRi0 <rioverosjustine92@gmail.com>
2026-02-28 05:14:35 +0800

    save config file

--- config.php
+++ config.php
@@ -10,6 +10,11 @@ define('BASE_URL', '');
 
 define('LANDBANK_URL', 'https://www.lbpemds.com/');
 
+// Google OAuth 2.0 settings for supplier login (fill these with your own values)
+define('GOOGLE_CLIENT_ID', '');
+define('GOOGLE_CLIENT_SECRET', '');
+// Example for local XAMPP: 'http://localhost/samples/SDO-SPMS/oauth_callback.php'
+define('GOOGLE_REDIRECT_URI', 'http://localhost/samples/SDO-SPMS/oauth_callback.php');
 
 if (session_status() === PHP_SESSION_NONE) {
     session_start();
